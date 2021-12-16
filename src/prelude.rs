1f78d675a4a1f77c042c22c72af7edf01b01bcdc
Varphone Wong <varphone@qq.com>
2021-12-16 16:02:18 +0800

    prelude: 增加 AmbientDetection, Gamma, HighDynamicRange
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,8 +1,11 @@
 pub use super::acm::AutoColorManagment;
 pub use super::ae::AutoExposure;
+pub use super::asd::AmbientDetection;
 pub use super::awb::AutoWhiteBalance;
 pub use super::context::Context;
 pub use super::error::XCamError;
+pub use super::gamma::Gamma;
+pub use super::hdr::HighDynamicRange;
 pub use super::nr::NoiseRemoval;
 pub use super::sharpen::Sharpen;
 pub use super::sysctl::SystemControl;
