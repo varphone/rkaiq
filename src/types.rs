5da3686fb1f39b9c47a5aaf79239c640f0e85352
Varphone Wong <varphone@qq.com>
2023-03-17 15:53:03 +0800

    新增摄像头模块信息相关类型
    Signed-off-by: Varphone Wong <varphone@qq.com>

--- src/types.rs
+++ src/types.rs
@@ -85,3 +85,91 @@ impl From<WorkingMode> for ffi::rk_aiq_working_mode_t {
         }
     }
 }
+
+/// 一个代表摄像头朝向的枚举。
+#[derive(Copy, Clone, Debug, Default)]
+pub enum CameraFacing {
+    #[default]
+    Back,
+    Front,
+    Left,
+    Right,
+}
+
+/// 一个代表摄像头朝向解析错误的枚举。
+#[derive(Copy, Clone, Debug)]
+pub enum CameraFacingParseError {
+    NotMatched,
+}
+
+impl std::str::FromStr for CameraFacing {
+    type Err = CameraFacingParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "b" | "back" => Ok(CameraFacing::Back),
+            "f" | "front" => Ok(CameraFacing::Front),
+            "l" | "left" => Ok(CameraFacing::Left),
+            "r" | "right" => Ok(CameraFacing::Right),
+            _ => Err(CameraFacingParseError::NotMatched),
+        }
+    }
+}
+
+/// 一个代表摄像头模块信息的类型。
+#[derive(Clone, Debug, Default)]
+pub struct CameraModuleInfo {
+    /// 摄像头模块编号。
+    pub index: usize,
+    /// 摄像头朝向。
+    pub facing: CameraFacing,
+    /// 摄像头模块名称。
+    pub name: String,
+    /// 摄像头模块所在的总线编号。
+    pub bus: usize,
+    /// 摄像头模块的通信地址。
+    pub reg: usize,
+}
+
+impl std::str::FromStr for CameraModuleInfo {
+    type Err = CameraModuleInfoParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let re = regex::Regex::new(
+            r#"m([[:xdigit:]]{2})_([bflr]{1})_([[:alnum:]]+)\s+([[:digit:]]+)-([[:xdigit:]]{4})"#,
+        )
+        .map_err(|_| CameraModuleInfoParseError::RegexError)?;
+
+        let caps = re
+            .captures(s)
+            .ok_or(CameraModuleInfoParseError::NotMatched)?;
+
+        let index = usize::from_str_radix(&caps[1], 16)
+            .map_err(|_| CameraModuleInfoParseError::ParseIndexFailed)?;
+        let facing = CameraFacing::from_str(&caps[2])
+            .map_err(|_| CameraModuleInfoParseError::ParseFacingFailed)?;
+        let name = caps[3].to_owned();
+        let bus = usize::from_str_radix(&caps[4], 16)
+            .map_err(|_| CameraModuleInfoParseError::ParseBusFailed)?;
+        let reg = usize::from_str_radix(&caps[5], 16)
+            .map_err(|_| CameraModuleInfoParseError::ParseRegFailed)?;
+        Ok(CameraModuleInfo {
+            index,
+            facing,
+            name,
+            bus,
+            reg,
+        })
+    }
+}
+
+/// 一个代表摄像头模块信息解析错误的枚举。
+#[derive(Copy, Clone, Debug)]
+pub enum CameraModuleInfoParseError {
+    ParseBusFailed,
+    ParseFacingFailed,
+    ParseIndexFailed,
+    ParseRegFailed,
+    NotMatched,
+    RegexError,
+}
